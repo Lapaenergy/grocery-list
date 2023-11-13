b9f3faf8cf637aafbeb5feace59dac8457eba755
Alex <lapaenergy@gmail.com>
2023-11-13 20:25:16 +0200

    initial project setup

---
+++ src/pages/product-details/product-details.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageTitle } from '../../components';
+
+const ProductDetailsPage = () => {
+  return (
+    <div>
+      <PageTitle titleText="Product Details" />
+    </div>
+  );
+};
+
+export default ProductDetailsPage;
